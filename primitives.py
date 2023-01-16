6606ee3d0966172e6a6bb3b3d26afecf69dcc426
Emily Fan <emilyfan@mit.edu>
2023-01-15 20:33:53 -0500

    add types to primitives

--- primitives.py
+++ primitives.py
@@ -1,41 +1,43 @@
+from typing import Any
 
 # number manipulations
 # alternatively we could just have integers in general be primitives
-def zero():
+def zero() -> int:
     return 0
 
 
-def pred(num: int):
+def pred(num: int) -> int:
     return num - 1
 
 
-def succ(num: int):
+def succ(num: int) -> int:
     return num + 1
 
-def neg(num: int):
+
+def neg(num: int) -> int:
     return -1 * num
 
 
-def eq(num1: int, num2: int):
+def eq(num1: int, num2: int) -> bool:
     return num1 == num2
 
 
-def less_than(num1: int, num2: int):
+def less_than(num1: int, num2: int) -> bool:
     return num1 < num2
 
 
 # boolean things
-def conj(bool1, bool2):
+def conj(bool1: bool, bool2: bool) -> bool:
     # and (conjunction)
     return bool1 and bool2
 
 
-def disj(bool1, bool2):
+def disj(bool1: bool, bool2: bool) -> bool:
     # or (disjunction)
     return bool1 or bool2
 
 
-def cond(bool_item, if_item, else_item):
+def cond(bool_item: bool, if_item: Any, else_item: Any) -> Any:
     # note that else_item may be None
     # note that both the if_item and else_item are run and evaluated
     #      before enterring this function
@@ -45,7 +47,7 @@ def cond(bool_item, if_item, else_item):
 
 
 # recursion
-def ind(input1, num_times: int, f):
+def ind(input1: Any, num_times: int, f: callable) -> Any:
     # ind stands for induction
 
     # can't use cond here because it will run ind even if num_times is 0
@@ -66,3 +68,4 @@ def ind(input1, num_times: int, f):
 #
 # def last(l: list):
 #     return l[-1]
+
