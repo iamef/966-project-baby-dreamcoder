1ba6dd13168a71beb79788fcaae345d03f91324d
Emily Fan <emilyfan@mit.edu>
2023-01-15 21:57:09 -0500

    abstraction skeleton

---
+++ abstraction.py
@@ -0,0 +1,16 @@
+
+# key: program
+# somehash = [
+# ]
+
+# return maxcount(hashcount)
+#
+#
+# def f(n):
+#     if(n=0): pass
+#     if (n=1): returns succ(n)
+#     if (n=2): pass
+#
+#
+# ind(b)(h)(succ(n)) =
+# ind(b)(h)(0) = h(ind(b)(b))
