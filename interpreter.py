7ed6ddda42f7de586717972674b219f17ce62759
Emily Fan <emilyfan@mit.edu>
2023-01-24 01:04:40 -0500

    have interpret take in variables (though sketchily as a string)

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,5 @@
 from typing import Any, List, Tuple
+import re
 
 # zero argument functions (zeroArgFunction, tuple())
 # one argument functions (oneArgFunc, (arg1,))
@@ -11,6 +12,12 @@ class Program:
     args: Tuple
 
     def __init__(self, func: callable, args: Tuple):
+        """
+
+        :param func: note the x_number has to be a direct arg of a program or subprogram
+                (can't be tucked into a tuple or list as of now)
+        :param args: if the args are variables, they can be named "x_number"
+        """
         if not callable(func) or type(args) is not tuple:
             raise TypeError("func must be a callable, and args must be a tuple")
 
@@ -18,11 +25,23 @@ class Program:
         self.args = args
 
 
-def interpret(prog: Program) -> Any:
+def interpret(prog: Program, inp=None) -> Any:
+    """
+
+    :param prog: note the x_number has to be a direct arg of a program or subprogram
+                (can't be tucked into a tuple or list as of now)
+    :param inp: used to fill in the "x_number" stuff...
+    :return:
+    """
     calculated_args: list = []
     for a in prog.args:
         if type(a) is not Program:  # kinda like base case
-            calculated_args.append(a)
+            # todo make this a little bit safer
+            if type(a) is str and re.fullmatch("^x_(\\d+)$", "x_0"):
+                inp_index = int(a.split("_")[1])
+                calculated_args.append(inp[inp_index])
+            else:
+                calculated_args.append(a)
         else:  # argument is a Program
             calculated_args.append(interpret(a))
 
