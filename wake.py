337d28e3638bc71a6582a7958da13b85fa1dd151
Emily Fan <emilyfan@mit.edu>
2023-01-25 00:57:12 -0500

    prim functions methods return List[callable] rather than List[tuple[string,callable]]

--- wake.py
+++ wake.py
@@ -47,7 +47,7 @@ def has_output_type(output_type: type, f: callable):
     # return len(type_overlap) > 0
 
 
-def get_functions_filtered(filt: Callable[[Tuple[Callable, str]], bool]) -> List[Tuple[str, callable]]:
+def get_functions_filtered(filt: Callable[[Tuple[Callable, str]], bool]) -> List[callable]:
     """
 
     :param filt: callable. input is a Tuple (function_name: string, function: callable)
@@ -61,10 +61,10 @@ def get_functions_filtered(filt: Callable[[Tuple[Callable, str]], bool]) -> List
     # todo in the future sort the functions by Bayesian probabilities
     funcs.sort(key=lambda f: (f[1].__code__.co_argcount, f[0]))
 
-    return funcs
+    return list(map(lambda f: f[1], funcs))
 
 
-def get_functions_by_types(input_type: Tuple[type, ...], output_type: type) -> List[Tuple[str, callable]]:
+def get_functions_by_types(input_type: Tuple[type, ...], output_type: type) -> List[callable]:
     """
     gets function with at least one of the input types
 
@@ -82,7 +82,7 @@ def get_functions_by_types(input_type: Tuple[type, ...], output_type: type) -> L
     return get_functions_filtered(is_right_type)
 
 
-def get_functions_by_output_type(output_type: type) -> List[Tuple[str, callable]]:
+def get_functions_by_output_type(output_type: type) -> List[callable]:
     return get_functions_filtered(
         # todo the is function is not necessary
         lambda f: inspect.isfunction(f) and has_output_type(output_type, f)
@@ -126,7 +126,7 @@ def generate_programs(prob: Problem, depth=5) -> List[Program]:
         # print(prog, valid_funcs[0])
         # print(prog.args, valid_funcs[0].args)
         # as you can see in this print statement,
-        # the prog and valid_funcs start will  matching because the inp update affects both of them
+        # the prog and valid_funcs will start matching because the inp update affects both of them
         # for the case prog = Program(lambda *args: args[inp]
         # print(interpret(prog, (42356, 1435, 123, 5, 176)), interpret(valid_funcs[0], (42356, 1435, 123, 5, 176)))
 
