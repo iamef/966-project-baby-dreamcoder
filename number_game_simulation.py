b9cc26d2c9e543dfc4061919614eba835f2703a9
Emily Fan <emilyfan@mit.edu>
2023-03-30 01:13:27 -0400

    add top programs analysis for adding more numbers to number game, add comments

--- number_game_simulation.py
+++ number_game_simulation.py
@@ -116,12 +116,25 @@ number_game_1 = Problem(
     ]
 )
 
-print("D1")
 
-# progs1_d1 = generate_programs(number_game_1, 1)
+
+
+
+
+
+
+
+################################################################
+# GRAPHS FOR NUMBER GAME WITH JUST NUMBER 60
+################################################################
+
+# Depth 1 program generation and graphs
+# print("D1")
+
+# progs1_d1 = generate_programs(number_game_1, 1, 1e-6)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d1)
 # make_plots(num_accepts, num_probs, "60", 1, 1e-6)
-#
+
 # progs1_d1 = generate_programs(number_game_1, 1, 1e-5)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d1)
 # make_plots(num_accepts, num_probs, "60", 1, 1e-5)
@@ -135,11 +148,12 @@ print("D1")
 # make_plots(num_accepts, num_probs, "60", 1, 1e-3)
 
 
-print("D2")
-
-progs1_d2 = generate_programs(number_game_1, 2)
-num_accepts, num_probs = get_accepts_probabilities(progs1_d2)
-make_plots(num_accepts, num_probs, "60", 2, 1e-6)
+# Depth 2 program generation and graphs
+# print("D2")
+#
+# progs1_d2 = generate_programs(number_game_1, 2, 1e-6)
+# num_accepts, num_probs = get_accepts_probabilities(progs1_d2)
+# make_plots(num_accepts, num_probs, "60", 2, 1e-6)
 #
 # progs1_d2 = generate_programs(number_game_1, 2, 1e-5)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d2)
@@ -154,8 +168,9 @@ make_plots(num_accepts, num_probs, "60", 2, 1e-6)
 # make_plots(num_accepts, num_probs, "60", 2, 1e-3)
 
 
-print("D3")
-
+# Depth 3 program generation and graphs
+# print("D3")
+#
 # progs1_d3 = generate_programs(number_game_1, 3, 1e-3)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d3)
 # make_plots(num_accepts, num_probs, "60", 3, 1e-3)
@@ -163,8 +178,21 @@ print("D3")
 # progs1_d3 = generate_programs(number_game_1, 3, 1e-4)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d3)
 # make_plots(num_accepts, num_probs, "60", 3, 1e-4)
+#
+# print("1e-5")
+# progs1_d3 = generate_programs(number_game_1, 3, 1e-5)
+# num_accepts, num_probs = get_accepts_probabilities(progs1_d3)
+# make_plots(num_accepts, num_probs, "60", 3, 1e-5)
+#
+# print("1e-6")
+# progs1_d3 = generate_programs(number_game_1, 3, 1e-6)
+# num_accepts, num_probs = get_accepts_probabilities(progs1_d3)
+# make_plots(num_accepts, num_probs, "60", 3, 1e-6)
+#
+# note that for depth 3, generate_program smaller than a 1e-4 threshold takes a significant amount of time.
 
 
+# Depth 4 program generation and graphs
 # print("D4")
 #
 # progs1_d4 = generate_programs(number_game_1, 4, 1e-3)
@@ -175,33 +203,98 @@ print("D3")
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d4)
 # make_plots(num_accepts, num_probs, "60", 4, 1e-4)
 #
-#
-# print("1e-5")
-# progs1_d3 = generate_programs(number_game_1, 3, 1e-5)
-# num_accepts, num_probs = get_accepts_probabilities(progs1_d3)
-# make_plots(num_accepts, num_probs, "60", 3, 1e-5)  # todo need to run this again
-#
 # progs1_d4 = generate_programs(number_game_1, 4, 1e-5)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d4)
 # make_plots(num_accepts, num_probs, "60", 4, 1e-5)
-
-
-# print("1e-6")
-# progs1_d3 = generate_programs(number_game_1, 3, 1e-6)
-# num_accepts, num_probs = get_accepts_probabilities(progs1_d3)
-# make_plots(num_accepts, num_probs, "60", 3, 1e-6)
 #
 # progs1_d4 = generate_programs(number_game_1, 4, 1e-6)
 # num_accepts, num_probs = get_accepts_probabilities(progs1_d4)
 # make_plots(num_accepts, num_probs, "60", 4, 1e-6)
+#
+# note that for depth 4, generate_program smaller than a 1e-4 threshold takes a significant amount of time.
 
 
 
 
-
-
-print("D2")
-
+# Use the size principle to get full Bayesian posterior
+# print("D2")
 # progs1_d2 = generate_programs(number_game_1, 2)
-# num_accepts, num_probs = get_weighted_accepts_probabilities(progs1_d2)
+# print(1, get_size_weighted_normalized(progs1_d2, 1)[:5])
+# num_accepts, num_probs = get_weighted_accepts_probabilities(progs1_d2, 1)
+# num_accepts, num_probs = get_weighted_accepts_probabilities(progs1_d2, 2)  # modified size principle
 # make_plots(num_accepts, num_probs, "60", 2, 1e-6)
+
+
+
+
+
+################################################################
+# TOP PROGRAMS FOR ADDING MORE NUMBERS TO NUMBER GAME
+################################################################
+
+# Using depth 2, 1e-6 settings.
+
+# 60, 65
+number_game_2 = Problem(
+    input_type=(int,),
+    output_type=bool,
+    input_ouput_pairs=[
+        ((60,), True),
+        ((65,), True)
+    ]
+)
+progs2_d2 = generate_programs(number_game_2, 2, 1e-6)
+
+# print top 5; technically prints Program object address
+# I used debugger to get the actual programs TODO can make it better later
+print(2, get_size_weighted_normalized(progs2_d2, 2)[:5])
+
+
+# 60, 65, 69
+number_game_3 = Problem(
+    input_type=(int,),
+    output_type=bool,
+    input_ouput_pairs=[
+        ((60,), True),
+        ((65,), True),
+        ((69,), True)
+    ]
+)
+
+progs3_d2 = generate_programs(number_game_3, 2, 1e-6)
+print(3, get_size_weighted_normalized(progs3_d2, 3)[:5])  # print top 5
+
+
+# 60-69
+number_game_60_69 = Problem(
+    input_type=(int,),
+    output_type=bool,
+    input_ouput_pairs=[
+        ((60,), True),
+        ((61,), True),
+        ((62,), True),
+        ((63,), True),
+        ((64,), True),
+        ((65,), True),
+        ((66,), True),
+        ((67,), True),
+        ((68,), True),
+        ((69,), True)
+    ]
+)
+
+progsf_d2 = generate_programs(number_game_60_69, 2, 1e-6)
+print("full", get_size_weighted_normalized(progsf_d2, 3)[:5])
+
+
+# try to get interesting conjunctions with depth 3
+
+# this took forever to run; I had it in the background probably for several hours and forgot about it
+# progsf_d3_10 = generate_programs(number_game_60_69, 3, 10e-10)
+# # random comment: technically count can get us the maximum a priori; using only size prinicple
+# progsf_d3_10_count = list(map(lambda x: (x[0], x[1], num_passes(x[0])), progsf_d3_10))
+# progsf_d3_10_count.sort(key=lambda x: x[2])  # sort by count, to investigate in debugger
+# print("full", get_size_weighted_normalized(progsf_d3_10, 10)[:5])
+
+#
+# print("phew... that was a lot")
